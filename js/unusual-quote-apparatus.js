319c0dc7f2feb3f0c812a5c93b27d752abfb7349
Carolina Knoll <carolina-knoll@hotmail.com>
2016-01-20 22:40:35 -0200

    Add new quotes to lucky quote machine

--- js/unusual-quote-apparatus.js
+++ js/unusual-quote-apparatus.js
@@ -1,19 +1,18 @@
-$(document).ready(function() {
- 
-  generateNewQuote();
+$(document).ready(function () {
 
-  function generateNewQuote(){
-    
-    var quotes = ["War brings death. And wounds and pain to both sides. There's nothing harder to accept, than the deaths of those you love. So you believe they could never die. Especially those who haven't known war. #Nagato, Naruto Shippuden", "Love is the reason why there is pain.  #Pain, Naruto Shippuden", "Learn to treasure your life because unfortunately, it can be taken away from you anytime. #L Lawliet, Death Note", "No matter how gifted you are... You, alone, cannot change the world. #L Lawliet, Death Note", "We have to change our way of thinking if we really want to change the future. #Watanabe Saki, From the New World", "I have to really enjoy the good things because it makes the bad things OK. #Emma Watson"];
-       
-    randomQuote = quotes[Math.floor(Math.random()*quotes.length)];
-    
-    quoteAuthor = randomQuote.split("#");
-    $('.saying').text(quoteAuthor[0]);
-    $('.author').text(quoteAuthor[1]);
-    
-  }
- 
-$("button").click(generateNewQuote);
-     
+    generateNewQuote();
+
+    function generateNewQuote() {
+        
+        var quotes = ["Luck? I don't know anything about luck. I've never banked on it and I'm afraid of people who do. Luck to me is something else: Hard work - and realizing what is opportunity and what isn't. #Lucille Ball", "Any fool can have bad luck; the art consists in knowing how to exploit it. #Frank Wedekind", "Chance favors the prepared mind. #Louis Pasteur", "The winds and waves are always on the side of the ablest navigators. #Edward Gibbon", "In the middle of every difficulty lies opportunity. #Albert Einstein", "Small opportunities are often the beginning of great enterprises. #Demosthenes", "I've found that luck is quite predictable. If you want more luck, take more chances. Be more active. Show up more often. #Brian Tracy", "I think luck falls on not just the brave but also the ones who believe they belong there. #Novak Djokovic", "Luck is a dividend of sweat. The more you sweat, the luckier you get. #Ray Kroc", "My success was due to good luck, hard work, and support and advice from friends and mentors. But most importantly, it depended on me to keep trying after I had failed. #Mark Warner"];
+
+        randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
+
+        quoteAuthor = randomQuote.split("#");
+        $('.saying').text(quoteAuthor[0]);
+        $('.author').text(quoteAuthor[1]);
+    }
+
+    $("button").click(generateNewQuote);
+
 });
