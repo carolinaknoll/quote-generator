6d69e08b2b296c6086337af2165219abd5d5c0ed
Carolina Knoll <carolinasknoll@gmail.com>
2019-09-17 09:51:26 -0300

    Remove unneeded react-router-dom dependency

--- src/index.js
+++ src/index.js
@@ -1,19 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {HashRouter, Route, Switch} from 'react-router-dom';
 import * as serviceWorker from './serviceWorker';
 import QuoteGenerator from '../src/containers/quote-generator';
 import './index.css';
 
 ReactDOM.render(
-  <HashRouter>
-    <div>
-      <Route exact path='/' component={QuoteGenerator} />
-      <Switch>
-        <Route path='/quote-generator' components={QuoteGenerator} />
-      </Switch>
-    </div>
-  </HashRouter>,
+  <QuoteGenerator />,
   document.getElementById('root'),
 );
 
