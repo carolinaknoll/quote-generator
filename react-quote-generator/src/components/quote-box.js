8db6ccf81fd144de1d14eb16c084d9a9141297c4
Carolina Knoll <carolinasknoll@gmail.com>
2019-09-17 09:06:54 -0300

    Remove jQuery and rewrite getNewQuote method

--- react-quote-generator/src/components/quote-box.js
+++ react-quote-generator/src/components/quote-box.js
@@ -1,27 +1,30 @@
 import React, {Component}  from 'react';
 import quotes from '../common/quotes';
-import $ from 'jquery';
 
 export default class SearchComponent extends Component {
-  generateQuote = () => {
+  getNewQuote = () => {
     let randomQuote = quotes[Math.floor(Math.random() * quotes.length)];
 
     let quoteAuthor = randomQuote.split("#");
-    $('.saying').text(quoteAuthor[0]);
-    $('.author').text(quoteAuthor[1]);
+    document.getElementById('text').textContent = quoteAuthor[0];
+    document.getElementById('author').textContent = quoteAuthor[1];
   }
 
   componentDidMount() {
-    this.generateQuote();
+    this.getNewQuote();
   }
 
 	render() {
     return (
       <div className="quote-box-container">
-        <button className="quote-button" id="new-quote" title="Get new quote!" onClick={this.generateQuote}>Try my luck!</button>
+        <button className="quote-button" id="new-quote" title="Get new quote!" onClick={this.getNewQuote}>
+          Try my luck!
+        </button>
+
         <div className="quote-box" id="quote-box">
-          <span className="saying" id="text"></span>
+          <span className="quote" id="text"></span>
           <span className="author" id="author"></span>
+
           <a id="tweet-quote" title="Tweet quote!" href="https://twitter.com/intent/tweet" target="_blank" rel="noopener noreferrer">
             <button className="quote-button" id="new-quote"><i className="fa fa-twitter"></i></button>
           </a>
