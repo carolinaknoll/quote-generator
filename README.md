fb9fe1c4687988062c4c5b309b90c6497210358d
Carolina Knoll <carolinasknoll@gmail.com>
2019-09-18 10:31:43 -0300

    Updates

--- README.md
+++
@@ -1,8 +0,0 @@
-#### :crystal_ball: Quote-Generator
-:octocat: Online quote generator where you click a button and it gives you a random quote. Created with HTML, CSS and a little bit of jQuery.   
-***
-The idea for this project came from FreeCodeCamp, where the project is one of a total of 9 front-end development related projects. 
-##### With this project, I learned to...   
-– Use RGB colors to add an opacity-like effect to the quotes background without affecting the other elements.  
-– Use HTML and CSS classes in the JavaScript code to make them update in real time when the user clicks the buttons.   
-– Use Math.floor() and Math.random() to randomize the quotes by length and show them randomly without following an order.
---
+++ precache-manifest.1c9f984f95ceba812af32e6b518c453a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93ebbb5533adc42bc66bee429b19b219",
+    "url": "/quote-generator/index.html"
+  },
+  {
+    "revision": "4190c36a1c3b20d34234",
+    "url": "/quote-generator/static/css/main.696eeebb.chunk.css"
+  },
+  {
+    "revision": "9eee67cf782a38d35aeb",
+    "url": "/quote-generator/static/js/2.d6a534e5.chunk.js"
+  },
+  {
+    "revision": "4190c36a1c3b20d34234",
+    "url": "/quote-generator/static/js/main.7a544de5.chunk.js"
+  },
+  {
+    "revision": "b42996cbb3818e11c29d",
+    "url": "/quote-generator/static/js/runtime~main.4226e67e.js"
+  },
+  {
+    "revision": "1fabbfc894a207741b2d7ab8d694ec4c",
+    "url": "/quote-generator/static/media/bg.1fabbfc8.jpg"
+  }
+]);
